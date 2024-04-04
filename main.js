eff81b7c53b87d6a011294d053c2129583239d9c
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-04-04 20:11:45 -0300

    tempo

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i<botoes.length; i++) {
-    botoes [i].onclick = function(){
+for(let i=0; i<botoes.length; i++){
+    botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
@@ -12,14 +11,13 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T23:59:59");
+const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
 
 let tempoAtual = new Date();
-let segundos = tempoFinal / 1000;
-let minutos = segundos / 60;
-let horas = minutos / 60;
-let dias = horas / 24; 
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
-let horas = Math.floor(horas / 24);
+let dias = Math.floor(horas / 24);
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = tempoFinal;
