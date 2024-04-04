01045c1356f23119addeb0832b37829766f8415a
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-04-04 20:25:03 -0300

    tempo

--- main.js
+++ main.js
@@ -24,4 +24,7 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = tempoFinal;
+contadores[0].textContent = dias + "dias" 
+                            + horas + "horas"
+                            + minutos + "minutos"
+                            + segundos + "segundos";
