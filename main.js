8ef87a1315f084648c6eb5af555eb56815461048
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-04-18 20:26:43 -0300

    ccontadores páginas

--- main.js
+++ main.js
@@ -20,6 +20,10 @@ const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
 ];
 
+for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo1){
